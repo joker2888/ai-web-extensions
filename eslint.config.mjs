3262ee035d91fabe239fa794a796dccfbb6df2e8
adamlui@protonmail.com <adamlui@protonmail.com>
2025-01-27 22:54:47 -0800

    Restored `htmlParser` to HTML `languageOptions` needed by GitHub Actions runner

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import css from '@eslint/css'
 import html from '@html-eslint/eslint-plugin'
+import htmlParser from '@html-eslint/parser'
 import importPlugin from 'eslint-plugin-import'
 import json from '@eslint/json'
 import markdown from '@eslint/markdown'
@@ -46,7 +47,7 @@ export default [
     { files: ['**/chatgpt.js'], languageOptions: { globals: { chatgpt: 'off' }}},
     { files: ['**/*.css'], language: 'css/css', ...css.configs.recommended },
     {
-        files: ['**/*.html'], plugins: { '@html-eslint': html },
+        files: ['**/*.html'], languageOptions: { parser: htmlParser }, plugins: { '@html-eslint': html },
         rules: {
             ...html.configs['flat/recommended'].rules,
             '@html-eslint/require-title': 'off', // allow missing title tags
