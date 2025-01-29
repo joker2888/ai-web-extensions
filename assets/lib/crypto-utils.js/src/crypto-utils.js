37e0d7d5aed5e203e5a78cba30b3f6eb4d1481d4
adamlui@protonmail.com <adamlui@protonmail.com>
2025-01-29 00:49:29 -0800

    Removed unneeded async/await from `generateSignature()`

--- assets/lib/crypto-utils.js/src/crypto-utils.js
+++ assets/lib/crypto-utils.js/src/crypto-utils.js
@@ -2,5 +2,5 @@
 
 window.cryptoUtils = {
     digestMessage(msg) { return CryptoJS.SHA256(msg).toString(CryptoJS.enc.Hex) },
-    async generateSignature({ time, msg, pkey }) { return await this.digestMessage(`${time}:${msg}:${pkey}`) }
+    generateSignature({ time, msg, pkey }) { return this.digestMessage(`${time}:${msg}:${pkey}`) }
 };
