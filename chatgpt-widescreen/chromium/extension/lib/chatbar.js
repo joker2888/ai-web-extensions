joker2888/ai-web-extensions

commit 3d68d4c6f4d56772a4df55d7c17f82aea4d22d7b

Stripped whitespace from `rgb()` functions

chatgpt-widescreen/chromium/extension/lib/chatbar.js
@@ -15,7 +15,7 @@ window.chatbar = {
         dark() {
             return chatbar.imports.site != 'chatgpt' ? undefined
                 : getComputedStyle(document.getElementById('composer-background') || document.documentElement)
-                    .backgroundColor == 'rgb(48, 48, 48)'
+                    .backgroundColor == 'rgb(48,48,48)'
         },
 
         tall() {

chatgpt-widescreen/firefox/extension/lib/chatbar.js
@@ -15,7 +15,7 @@ window.chatbar = {
         dark() {
             return chatbar.imports.site != 'chatgpt' ? undefined
                 : getComputedStyle(document.getElementById('composer-background') || document.documentElement)
-                    .backgroundColor == 'rgb(48, 48, 48)'
+                    .backgroundColor == 'rgb(48,48,48)'
         },
 
         tall() {

amazongpt/greasemonkey/amazongpt.user.js
@@ -3,7 +3,7 @@
 // @description            Adds the magic of AI to Amazon shopping
 // @author                 KudoAI
 // @namespace              https://kudoai.com
-// @version                2025.2.16
+// @version                2025.2.16.1
 // @license                MIT
 // @icon                   https://amazongpt.kudoai.com/assets/images/icons/amazongpt/black-gold-teal/icon48.png?v=0fddfc7
 // @icon64                 https://amazongpt.kudoai.com/assets/images/icons/amazongpt/black-gold-teal/icon64.png?v=0fddfc7
@@ -546,7 +546,7 @@
             const stateStyles = {
                 on: {
                     light: 'color: #5cef48 ; text-shadow: rgba(255,250,169,0.38) 2px 1px 5px',
-                    dark:  'color: #5cef48 ; text-shadow: rgb(55, 255, 0) 3px 0 10px'
+                    dark:  'color: #5cef48 ; text-shadow: rgb(55,255,0) 3px 0 10px'
                 },
                 off: {
                     light: 'color: #ef4848 ; text-shadow: rgba(255,169,225,0.44) 2px 1px 5px',

autoclear-chatgpt-history/greasemonkey/autoclear-chatgpt-history.user.js
@@ -225,7 +225,7 @@
 // @description:zu      Ziba itshala lokucabanga okuzoshintshwa ngokuzenzakalelayo uma ukubuka chatgpt.com
 // @author              Adam Lui
 // @namespace           https://github.com/adamlui
-// @version             2025.2.16
+// @version             2025.2.16.1
 // @license             MIT
 // @icon                https://assets.autoclearchatgpt.com/images/icons/openai/black/icon48.png?v=f461c06
 // @icon64              https://assets.autoclearchatgpt.com/images/icons/openai/black/icon64.png?v=f461c06
@@ -481,7 +481,7 @@
             const stateStyles = {
                 on: {
                     light: 'color: #5cef48 ; text-shadow: rgba(255,250,169,0.38) 2px 1px 5px',
-                    dark:  'color: #5cef48 ; text-shadow: rgb(55, 255, 0) 3px 0 10px'
+                    dark:  'color: #5cef48 ; text-shadow: rgb(55,255,0) 3px 0 10px'
                 },
                 off: {
                     light: 'color: #ef4848 ; text-shadow: rgba(255,169,225,0.44) 2px 1px 5px',

bravegpt/greasemonkey/bravegpt.user.js
@@ -148,7 +148,7 @@
 // @description:zu        Yengeza izimpendulo ze-AI ku-Brave Search (inikwa amandla yi-GPT-4o!)
 // @author                KudoAI
 // @namespace             https://kudoai.com
-// @version               2025.2.16
+// @version               2025.2.16.1
 // @license               MIT
 // @icon                  https://assets.bravegpt.com/images/icons/bravegpt/icon48.png?v=df624b0
 // @icon64                https://assets.bravegpt.com/images/icons/bravegpt/icon64.png?v=df624b0
@@ -717,7 +717,7 @@
             const stateStyles = {
                 on: {
                     light: 'color: #5cef48 ; text-shadow: rgba(255,250,169,0.38) 2px 1px 5px',
-                    dark:  'color: #5cef48 ; text-shadow: rgb(55, 255, 0) 3px 0 10px'
+                    dark:  'color: #5cef48 ; text-shadow: rgb(55,255,0) 3px 0 10px'
                 },
                 off: {
                     light: 'color: #ef4848 ; text-shadow: rgba(255,169,225,0.44) 2px 1px 5px',

chatgpt-auto-continue/chromium/extension/content.js
@@ -54,7 +54,7 @@
             const stateStyles = {
                 on: {
                     light: 'color: #5cef48 ; text-shadow: rgba(255,250,169,0.38) 2px 1px 5px',
-                    dark:  'color: #5cef48 ; text-shadow: rgb(55, 255, 0) 3px 0 10px'
+                    dark:  'color: #5cef48 ; text-shadow: rgb(55,255,0) 3px 0 10px'
                 },
                 off: {
                     light: 'color: #ef4848 ; text-shadow: rgba(255,169,225,0.44) 2px 1px 5px',

chatgpt-auto-continue/chromium/extension/popup/style.css
@@ -49,7 +49,7 @@ a:focus, a:hover { text-decoration: underline ; color: inherit }
     display: flex ; min-height: 2rem ; padding: 0 14px 0 2px ; white-space: nowrap ; font-size: 91%
 }
 .menu-icon { padding: 8px }
-.menu-area:focus, .menu-area:hover { background: rgb(100, 149, 237) } /* add hover color/cursor */
+.menu-area:focus, .menu-area:hover { background: rgb(100,149,237) } /* add hover color/cursor */
 .menu-item:hover span:not(.slider) { filter: invert(1) } /* invert setting labels on hover */
 .menu-item > label > .slider { transform: scale(0.95) ; top: 1px } /* make child toggles smaller */
 .menu-prompt { margin-left: 2px } /* align non-toggle items */

chatgpt-auto-continue/firefox/extension/content.js
@@ -54,7 +54,7 @@
             const stateStyles = {
                 on: {
                     light: 'color: #5cef48 ; text-shadow: rgba(255,250,169,0.38) 2px 1px 5px',
-                    dark:  'color: #5cef48 ; text-shadow: rgb(55, 255, 0) 3px 0 10px'
+                    dark:  'color: #5cef48 ; text-shadow: rgb(55,255,0) 3px 0 10px'
                 },
                 off: {
                     light: 'color: #ef4848 ; text-shadow: rgba(255,169,225,0.44) 2px 1px 5px',

commit 1bbb33786eb119dc429ab642b10182dd23d7da55

Fixed https://github.com/adamlui/ai-web-extensions/commit/3d68d4c6f4d56772a4df55d7c17f82aea4d22d7b broke `chatbar.is.dark()` detection

chatgpt-widescreen/chromium/extension/lib/chatbar.js
@@ -15,7 +15,7 @@ window.chatbar = {
         dark() {
             return chatbar.imports.site != 'chatgpt' ? undefined
                 : getComputedStyle(document.getElementById('composer-background') || document.documentElement)
-                    .backgroundColor == 'rgb(48,48,48)'
+                    .backgroundColor == 'rgb(48, 48, 48)'
         },
 
         tall() {

chatgpt-widescreen/firefox/extension/lib/chatbar.js
@@ -15,7 +15,7 @@ window.chatbar = {
         dark() {
             return chatbar.imports.site != 'chatgpt' ? undefined
                 : getComputedStyle(document.getElementById('composer-background') || document.documentElement)
-                    .backgroundColor == 'rgb(48,48,48)'
+                    .backgroundColor == 'rgb(48, 48, 48)'
         },
 
         tall() {