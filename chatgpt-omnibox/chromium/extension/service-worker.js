a387f1aede9c70dc6ec20c80eef9e35b175ea62f
Adam Lui <adam@kudoai.com>
2025-02-16 18:11:14 -0800

    Abstracted base URLs

--- chatgpt-omnibox/chromium/extension/service-worker.js
+++ chatgpt-omnibox/chromium/extension/service-worker.js
@@ -1,7 +1,8 @@
-// Launch chatgpt.com on toolbar icon click
-chrome.action.onClicked.addListener(() =>
-    chrome.tabs.create({ url: 'https://chatgpt.com' }))
+const chatgptURL = 'https://chatgpt.com'
+
+// Launch ChatGPT on toolbar icon click
+chrome.action.onClicked.addListener(() => chrome.tabs.create({ url: chatgptURL }))
 
 // Query ChatGPT on omnibox query submitted
 chrome.omnibox.onInputEntered.addListener(query =>
-    chrome.tabs.update({ url: `https://chatgpt.com/?q=${decodeURIComponent(query)}` }))
+    chrome.tabs.update({ url: `${chatgptURL}/?q=${decodeURIComponent(query)}` }))
