20f3d5a425d8c9243d87df3f205b626cc254e23c
Adam Lui <adam@kudoai.com>
2025-02-16 17:58:29 -0800

    Changed `chatbar` global to writable

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,7 +17,7 @@ export default [
         languageOptions: {
             ecmaVersion: 'latest', sourceType: 'script',
             globals: {
-                ...globals.browser, ...globals.greasemonkey, ...globals.node, buttons: 'writable', chatbar: 'readonly',
+                ...globals.browser, ...globals.greasemonkey, ...globals.node, buttons: 'writable', chatbar: 'writable',
                 chatgpt: 'readonly', chrome: 'readonly', config: 'writable', CryptoJS: 'readonly',
                 cryptoUtils: 'readonly', deepseek: 'readonly', dom: 'readonly', GM_cookie: 'readonly', hljs: 'readonly',
                 icons: 'writable', ipv4: 'readonly', marked: 'readonly', modals: 'writable',
