5e8b9b4df7be7357c77ec92e1d9b6348a3620239
Adam Lui <adam@kudoai.com>
2025-02-16 18:22:49 -0800

    Condensed listeners

--- chatgpt-omnibox/firefox/extension/background.js
+++ chatgpt-omnibox/firefox/extension/background.js
@@ -1,3 +1,2 @@
-// Launch chatgpt.com on toolbar icon click
-chrome.action.onClicked.addListener(() =>
-    chrome.tabs.create({ url: 'https://chatgpt.com' }))
+// Launch ChatGPT on toolbar icon click
+chrome.action.onClicked.addListener(() => chrome.tabs.create({ url: 'https://chatgpt.com' }))
