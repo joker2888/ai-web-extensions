a5b62907d4f9e3402e782c00daf05e9cd0297156
Adam Lui <adam@kudoai.com>
2025-03-06 02:27:15 -0800

    Added `JSON5` to globals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,7 +20,7 @@ export default [
                 ...globals.browser, ...globals.greasemonkey, ...globals.node, buttons: 'writable', chatbar: 'writable',
                 chatgpt: 'readonly', chrome: 'readonly', config: 'writable', CryptoJS: 'readonly',
                 cryptoUtils: 'readonly', deepseek: 'readonly', dom: 'readonly', GM_cookie: 'readonly', hljs: 'readonly',
-                icons: 'writable', ipv4: 'readonly', marked: 'readonly', modals: 'writable',
+                icons: 'writable', JSON5: 'readonly', ipv4: 'readonly', marked: 'readonly', modals: 'writable',
                 renderMathInElement: 'readonly', settings: 'writable', toggles: 'writable', tooltip: 'writable',
                 ui: 'readonly'
             }
