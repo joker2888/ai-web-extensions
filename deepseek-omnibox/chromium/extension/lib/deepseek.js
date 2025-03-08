4c922d720669fb1b2743908835fa8cf6c2429be3
Adam Lui <adam@kudoai.com>
2025-03-08 10:44:43 -0800

    Removed unused selectors/methods, improved `send()` to wait for button enabled, eliminated multi-exports

--- deepseek-omnibox/chromium/extension/lib/deepseek.js
+++ deepseek-omnibox/chromium/extension/lib/deepseek.js
@@ -1,32 +1,21 @@
-const deepseek = {
-    selectors: {
-        btns: {
-            newChat: 'div:has(> div > svg path[d^="M9.10999"])',
-            send: 'div[role=button]:has([d^="M7 16c-.595 0-1.077-"])'
-        },
-        chatbox: 'chat-input'
-    },
+window.deepseek = {
+    selectors: { btns: { send: 'div[role=button]:has([d^="M7 16c-.595 0-1.077-"])' }, chatbox: '#chat-input' },
 
-    getChatbox() { return document.getElementById(this.selectors.chatbox) },
-    getNewChatButton() { return document.querySelector(this.selectors.btns.newChat) },
+    getChatbox() { return document.querySelector(this.selectors.chatbox) },
     getSendButton() { return document.querySelector(this.selectors.btns.send) },
 
-    send(msg) {
+    async send(msg) {
         if (typeof arguments[0] != 'string') return console.error(`Argument 'msg' must be a string!`)
         const textArea = this.getChatbox() ; if (!textArea) return console.error('Chatbox not found!')
         textArea.setRangeText(msg) ; textArea.dispatchEvent(new Event('input', { bubbles: true }))
-        this.getSendButton()?.click()
-    },
-
-    sendInNewChat(msg) {
-        if (typeof msg != 'string') return console.error('Message must be a string!')
-        try { this.getNewChatButton().click() } catch (err) { return console.error(err.message) }
-        setTimeout(() => this.send(msg), 500)
-    },
-
-    startNewChat() { try { this.getNewChatButton().click() } catch (err) { console.error(err.message) }}
+        new Promise(resolve => { // wait for Send button to enable
+            const sendBtnSelector = this.selectors.btns.send + '[aria-disabled=false]',
+                  sendBtn = document.querySelector(sendBtnSelector)
+            if (sendBtn) resolve(sendBtn)
+            else new MutationObserver((_, obs) => {
+                const sendBtn = document.querySelector(sendBtnSelector)
+                if (sendBtn) { obs.disconnect() ; resolve(sendBtn) }
+            }).observe(this.getChatbox(), { childList: true, subtree: true })
+        }).then(btn => btn.click())
+    }
 }
-
-// Export deepseek object
-try { window.deepseek = deepseek } catch (err) {} // for Greasemonkey
-try { module.exports = deepseek } catch (err) {} // for CommonJS
