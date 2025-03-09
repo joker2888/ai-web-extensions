4b323418e2653ab257fae369fac0fe4d8dc2b64a
Adam Lui <adam@kudoai.com>
2025-03-09 03:11:11 -0700

    Reverted https://github.com/adamlui/ai-web-extensions/commit/638862aa314842a56aefefcf7735556d88aa09e0 due to site verification required, added `activeTab` permission + logging to test published CRX

--- deepseek-omnibox/chromium/extension/content.js
+++ deepseek-omnibox/chromium/extension/content.js
@@ -1,5 +1,8 @@
 (async () => {
     for (const resource of ['lib/deepseek.js', 'lib/dom.js']) await import(chrome.runtime.getURL(resource))
-    chrome.runtime.onMessage.addListener(
-        query => dom.get.loadedElem(deepseek.selectors.chatbox).then(() => deepseek.send(query)))
+    console.log('JS resources loaded')
+    chrome.runtime.onMessage.addListener(query =>
+        dom.get.loadedElem(deepseek.selectors.chatbox)
+            .then(() => { console.log('chatbox loaded') ; deepseek.send(query) })
+    )
 })()
